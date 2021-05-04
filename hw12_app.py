c39ba139a54b29aaba5e7735bc15057318d005df
irchorn <70407458+irchorn@users.noreply.github.com>
2021-05-03 22:23:33 -0400

    Update hw12_app.py

--- hw12_app.py
+++ hw12_app.py
@@ -1,18 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[49]:
-
-
 from flask import Flask, render_template, redirect, url_for, request, session, flash
 from flask import g
 import sqlite3
 import datetime
 
 app = Flask(__name__)
-
-
-# In[45]:
 
 
 def init_db():
@@ -23,12 +14,15 @@ def init_db():
         db.commit()
 
 
-# In[46]:
-
-
 app.config['SECRET_KEY']='dev'
 
 DATABASE = 'hw12.db'
+
+def connect_db():
+   
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -43,23 +37,14 @@ def close_connection(exception):
         db.close()
 
 
-# In[47]:
-
-
 @app.before_request
 def before_request():
     g.db = connect_db()
 
 
-# In[48]:
-
-
 @app.route('/')
 def index():
     return rederect('/login')
-
-
-# In[59]:
 
 
 @app.route('/login', method =['GET', 'POST'])
@@ -77,27 +62,21 @@ def login():
    
 
 
-# In[60]:
-
-
-@app.route('/dashboard/')
+@app.route('/dashboard/', method =['GET', 'POST'])
 def dashboard():
     if session['username']=='admin':
         cur = g.db.execute("SELECT * FROM Students")
         cur2 = g.db.execute("SELECT * FROM Quizzes")
         
-        result = cur.fetchall()
-        result2 = cur2.fetchall()
+        res = cur.fetchall()
+        res2 = cur2.fetchall()
         
-        students = [dict(student_id = r[0], first_name = r[1], last_name =r[2]) for r in result]
-        quizzes = [dict(quiz_id = r[0], subject = r[1], number_of_questions=r[2], date=r[3]) for r in result2]
+        students = [dict(student_id = r[0], first_name = r[1], last_name =r[2]) for r in res]
+        quizzes = [dict(quiz_id = r[0], subject = r[1], number_of_questions=r[2], date=r[3]) for r in res2]
         
         return render_template("dashboard.html", students=students, quizzes=quizzes)
     else:
-        return redirect('/login')
-
-
-# In[61]:
+        return redirect(url_for('/login'))
 
 
 @app.route('/add_student', method =['GET', 'POST'])
@@ -109,14 +88,14 @@ def add_student():
             g.db=get.db()
       
             g.db.execute("INSERT INTO Students(last_name) values(?,)",(student_name))
-        return redirect(url_for('dashboard'))
+            g.db.commit()
+            flash(error)
+        return render_template("add_student.html") 
+      else:
+        return redirect(url_for('/dashboard'))
     
 
-
-# In[66]:
-
-
-@app.route('/add_quiz', method =['POST'])
+@app.route('/add_quiz', method =['GET','POST'])
 def add_quiz():
     if session ['user_name'] == 'admin':
     
@@ -125,11 +104,35 @@ def add_quiz():
             g.db=get.db()
       
             g.db.execute("INSERT INTO Quizzes(subject) values(?,)",(subject))
+            g.db.commit()
+            flash(error) 
+        return render_template("add_quiz.html")
+     else:
+        return redirect(url_for('/dashboard'))
+    
+@app.route('/add_score', method == ['GET','POST'])
+def add_score():
+    if session ['user_name'] == 'admin':
+    
+        if request.metod == 'POST':
+            score = request.form['score']
+            g.db=get.db()
+      
+            g.db.execute("INSERT INTO Score(score) values(?,)",(score))
+            g.db.commit()
+            flash(error)
+        return render_template("add_score.html")
+     else:
         return redirect(url_for('dashboard'))
+    
 
 
-# In[ ]:
+@app.rout('/student/<id>')
+def view_results(id):
+    cur = g.db.execute("SELECT Quizzes.q_id, Score.score, Quizzes.date, Quizzes.subject FROM Students JOIN Score ON Score.q_id = Quizzes.q_id WHERE Students.s_id=?",[id])
+    res = cur.fetchall()
+    student_results = dict[q_id=r[0],subject=r[1], date=r[2], score=r[3] ]
+    return render template("score.html", student_results=student_results) 
 
-
-
-
+if __name__ == "__main__":
+    app.run()
